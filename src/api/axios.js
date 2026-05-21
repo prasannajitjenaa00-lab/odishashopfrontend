3388bdf5803d66fe5f388d3ddb9cc520e60c8c88
PRASANNAJIT JENA <prasannajitjenaa00@gmail.com>
2026-05-21 21:31:31 +0530

    update changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  // baseURL:"https://odsiahashopbackend.onrender.com/api",
-  baseURL: 'http://localhost:5000/api', // Updated to local server
+  baseURL:"https://odsiahashopbackend.onrender.com/api",
+  // baseURL: 'http://localhost:5000/api', // Updated to local server
   headers: { 'Content-Type': 'application/json' }
 })
 
